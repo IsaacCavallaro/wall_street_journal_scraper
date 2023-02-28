1dfbc5ce90bbfe0b02ee319d9116a1d5f7e7746a
isaaccavallaro <isaaccavallaro@gmail.com>
2023-03-01 08:50:05 +1000

    WIP: Successfully scrapping headlines

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -5,9 +5,11 @@ from bs4 import BeautifulSoup
 class WSJScraper:
     def __init__(self, url):
         self.url = url
-
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        
     def _get_html(self):
-        response = requests.get(self.url)
+        response = requests.get(self.url, headers=self.headers)
         if response.status_code == 200:
             return response.text
         else:
@@ -33,7 +35,6 @@ class WSJScraper:
 
     def scrape(self):
         html = self._get_html()
-        print(html)
         if html is not None:
             soup = BeautifulSoup(html, 'html.parser')
             try:
