03daecd97ea0ca8a16d66cf55890dbf09556059b
isaaccavallaro <isaaccavallaro@gmail.com>
2023-03-01 09:18:30 +1000

    BUG: scrapping dollar price issue

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,28 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 class WSJScraper:
-    def __init__(self, url):
+    def __init__(self, url, headers):
         self.url = url
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        
-    def _get_html(self):
-        response = requests.get(self.url, headers=self.headers)
+        self.headers = headers
+
+    def _get_html(self, url):
+        response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
-            return response.text
+            html = response.text
+            return html
         else:
             return None
 
     def _get_price(self, soup):
-        price_tag = soup.select_one('.WSJBase--column--10-5-3nD2q5gH')
+        price_tag = soup.select_one('.WSJTheme--headline--unZqjb45:contains("US$")')
         if price_tag is not None:
-            price_str = price_tag.text.strip()
-            price = float(price_str.replace(',', ''))
-            return price
-        else:
-            return None
+            price_str = re.search(r'US\$\d+(\.\d{1,2})?', price_tag.text)
+            if price_str is not None:
+                price = float(price_str.group().replace('US$', '').replace(',', ''))
+                return price
+        return None
 
     def _get_headlines(self, soup):
         headlines = []
@@ -34,23 +35,38 @@ class WSJScraper:
         return headlines
 
     def scrape(self):
-        html = self._get_html()
-        if html is not None:
-            soup = BeautifulSoup(html, 'html.parser')
+        headlines_url = self.url
+        dollar_price_url = 'https://www.wsj.com/market-data/quotes/index/DXY'
+
+        headlines_html = self._get_html(headlines_url)
+        if headlines_html is not None:
+            headlines_soup = BeautifulSoup(headlines_html, 'html.parser')
             try:
-                price = self._get_price(soup)
-                headlines = self._get_headlines(soup)
+                headlines = self._get_headlines(headlines_soup)
             except Exception:
                 return None
-            else:
-                return {'price': price, 'headlines': headlines}
         else:
             return None
+
+        dollar_price_html = self._get_html(dollar_price_url)
+        if dollar_price_html is not None:
+            dollar_price_soup = BeautifulSoup(dollar_price_html, 'html.parser')
+            try:
+                price = self._get_price(dollar_price_soup)
+            except Exception:
+                return None
+        else:
+            return None
+
+        return {'price': price, 'headlines': headlines}
 
 
 def main():
     url = "https://www.wsj.com/"
-    scraper = WSJScraper(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    scraper = WSJScraper(url, headers)
     result = scraper.scrape()
     if result is not None:
         dollar_price = result['price']
